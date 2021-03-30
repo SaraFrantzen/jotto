1cc2d6f1aee6c1a0ec0d92efd6dd6dce3652e051
Sara lundkvist <saralundkvist86@gmail.com>
2021-03-30 11:39:11 +0200

    creates congrats component and test

---
+++ src/Congrats.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Congrats = ({ success }) => {
+  return (
+    <>
+      {success ? (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+      ) : (
+        <div data-test="component-congrats" />
+      )}
+    </>
+  );
+};
+
+export default Congrats;
