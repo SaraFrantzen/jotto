88f8e5115e7fb7dd38ea31b06f9908033e23540f
Sara lundkvist <saralundkvist86@gmail.com>
2021-04-11 13:24:16 +0200

    set up guessWords test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 export const actionTypes = {
-    CORRECT_GUESS: "CORRECT_GUESS",
-}
+  CORRECT_GUESS: "CORRECT_GUESS",
+  GUESS_WORD: "GUESS_WORD",
+};
 
-export function correctGuess() {
-    return {type: actionTypes.CORRECT_GUESS}
-}
+//Takes the guessed word and returns a function
+export const guessWord = (guessedWord) => {
+  return function (dispatch, getState) {
+    
+  };
+};
 
 export const getSecretWord = () => {
   return axios.get("http://localhost:3030").then((response) => response.data);
